1365c8ac0bf4adcbe2d46cfd3d5414bac7c4f854
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-24 14:10:07 +0530

    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Angular_App';
   imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuy4k1Dyy4HeB5W8q_0ul3Cgcu0d2OVNvjbQ&s";
+  url="https://www.bridgelabz.com";
+
+  onClick(event:MouseEvent){
+    
+    window.open(this.url,"_blank");
+  }
 }
