c1a604e61309e0f96c3cd76906e2ef9319109184
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-24 15:38:45 +0530

    UC5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -14,9 +14,19 @@ export class AppComponent {
   imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuy4k1Dyy4HeB5W8q_0ul3Cgcu0d2OVNvjbQ&s";
   url="https://www.bridgelabz.com";
   userName="Ashish"
-
+  nameError=""
   onClick(event:MouseEvent){
-    
+
     window.open(this.url,"_blank");
   }
+  onInput(): void { // Removed event parameter, not needed
+    const namePattern = /^[A-Z][a-zA-Z\s]{2,}$/; // Starts with uppercase, min 3 chars
+
+    if (!this.userName.match(namePattern)) {
+      this.nameError = "Name must start with a capital letter and have at least 3 characters.";
+    } else {
+      this.nameError = ""; // Clear error if valid
+    }
+  }
+ 
 }
